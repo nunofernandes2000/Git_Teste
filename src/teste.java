a1051d0c102543856ecdf4e61e8c295c1023d3a1
Ana Fernandes <21635@ipportalegre.pt>
2022-08-19 21:12:48 +0100

    teste nº2

--- src/teste.java
+++ src/teste.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class teste {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+        Scanner sc = new Scanner(System.in);
+        String name;
+        int idade;
+
+        System.out.println("Bme vindo, qual é o teu nome?");
+        name = sc.next();
+
+        System.out.println("Bme vindo, qual é a tua idade?");
+        idade = sc.nextInt();
     }
 }
